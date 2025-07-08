9186f9de03d97213ea8e9b3f8824030d04a858f1
lordgabu <vinodsharma98176@gmail.com>
2025-07-08 21:07:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+import os
+from pyrogram import Client as AFK, idle
+from pyrogram.enums import ChatMemberStatus, ChatMembersFilter
+from pyrogram import enums
+from pyrogram.types import ChatMember
+import asyncio
+import logging
+import tgcrypto
+from pyromod import listen
+import logging
+from tglogging import TelegramLogHandler
+from aiohttp import web
+
+# Config 
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7979826252:AAG6PkktURFL-udAd3KipzwiFXp6FEQDbCg")
+    API_ID = int(os.environ.get("API_ID",  "23442913"))
+    API_HASH = os.environ.get("API_HASH", "864a97e16b4ff7dc65ff5e2d1549b4a2")
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    SESSIONS = "./SESSIONS"
+
+    AUTH_USERS = os.environ.get('AUTH_USERS', '7841326954').split(',')
+    for i in range(len(AUTH_USERS)):
+        AUTH_USERS[i] = int(AUTH_USERS[i])
+
+    GROUPS = os.environ.get('GROUPS', '-1002300391155').split(',')
+    for i in range(len(GROUPS)):
+        GROUPS[i] = int(GROUPS[i])
+
+    LOG_CH = os.environ.get("LOG_CH", "-1002381344447")
+
+# TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        TelegramLogHandler(
+            token=Config.BOT_TOKEN, 
+            log_chat_id= Config.LOG_CH, 
+            update_interval=2, 
+            minimum_lines=1, 
+            pending_logs=200000),
+        logging.StreamHandler()
+    ]
+)
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.info("live log streaming to telegram.")
+
+
+# Store
+class Store(object):
+    CPTOKEN = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0"
+    SPROUT_URL = "https://discuss.oliveboard.in/"
+    ADDA_TOKEN = ""
+    THUMB_URL = "https://telegra.ph/file/84870d6d89b893e59c5f0.jpg"
+
+# Format
+class Msg(object):
+    START_MSG = "**/pro**"
+
+    TXT_MSG = "Hey <b>{user},"\
+        "\n\n`I'm Ankit Shakya Robot. I Can Download Many Type of Links.`"\
+            "\n\nSend a TXT or HTML file :-</b>"
+
+    ERROR_MSG = "<b>DL Failed ({no_of_files}) :-</b> "\
+        "\n\n<b>Name: </b>{file_name},\n<b>Link:</b> `{file_link}`\n\n<b>Error:</b> {error}"
+
+    SHOW_MSG = "<b>Downloading :- "\
+        "\n`{file_name}`\n\nLink :- `{file_link}`</b>"
+
+    CMD_MSG_1 = "`{txt}`\n\n**Total Links in File are :-** {no_of_links}\n\n**Send any Index From `[ 1 - {no_of_links} ]` :-**"
+    CMD_MSG_2 = "<b>Uploading :- </b> `{file_name}`"
+    RESTART_MSG = "✅ HI Bhai log\n✅ PATH CLEARED"
+
+app = web.Application()
+routes = web.RouteTableDef()
+
+@routes.get("/")
+async def home(request):
+    return web.Response(text="Hello, Docker!")
+
+app.add_routes(routes)
+
+# Get the port from environment variables (default: 8080)
+PORT = int(os.getenv("PORT", 8080))
+
+if __name__ == "__main__":
+    web.run_app(app, host="0.0.0.0", port=PORT)
+   
